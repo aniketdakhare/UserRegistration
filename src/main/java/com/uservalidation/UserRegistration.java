5f51b50cd2ff6194b4386fb34f3f7966e1617b33
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-19 01:37:11 +0530

    ADDED method to validate First Name

--- src/main/java/com/uservalidation/UserRegistration.java
+++ src/main/java/com/uservalidation/UserRegistration.java
@@ -1,10 +1,23 @@
 package com.uservalidation;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration
 {
+    /**
+     * PATTERN FOR NAME
+     */
+    private static final String NAME_PATTERN = "^[A-Z][A-Za-z0-9]{2,}$";
 
-    public String welcomeMassage(String massage)
+    /**
+     * METHOD TO VALIDATE FIRST NAME
+     * @param firstName takes First Name for validation
+     * @return value as true
+     */
+    public boolean validateFirstName(String firstName)
     {
-        return "Welcome to User Registration Program";
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        return pattern.matcher(firstName).matches();
     }
 }
+

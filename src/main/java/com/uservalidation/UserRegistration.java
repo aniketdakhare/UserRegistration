91250d6d42e6e064e0d48bedf8714f2392f194db
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-19 04:00:29 +0530

    ADDED method to validate Email and ADDED test class to test email

--- src/main/java/com/uservalidation/UserRegistration.java
+++ src/main/java/com/uservalidation/UserRegistration.java
@@ -5,9 +5,10 @@ import java.util.regex.Pattern;
 public class UserRegistration
 {
     /**
-     * PATTERN FOR NAME
+     * PATTERN FOR NAME AND EMAIL
      */
     private static final String NAME_PATTERN = "^[A-Z][A-Za-z0-9]{2,}$";
+    private static final String EMAIL_PATTERN = "^[0-9a-zA-Z]+([.+-]?[0-9a-zA-z])*[@][a-zA-Z0-9]+([.][a-zA-Z]{2,}){1,2}$";
 
     /**
      * METHOD TO VALIDATE FIRST NAME
@@ -29,6 +30,17 @@ public class UserRegistration
     {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matcher(lastName).matches();
+    }
+
+    /**
+     * METHOD TO VALIDATE EMAIL ID
+     * @param email takes Email Id for validation
+     * @return value as true
+     */
+    public boolean validateEmail(String email)
+    {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(email).matches();
     }
 }
 
